816720d52fc5394d25a5a24b58b424e9d6b5828b
Julianka-GJ <julia.garmarchuk@gmail.com>
2022-08-18 09:52:54 +0300

    Page added Main , component created ItemCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.scss';
 import Header from "./Components/Header/Header.jsx";
+import Main from "./Screens/Main/Main";
 import Footer from "./Components/Footer/Footer.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
