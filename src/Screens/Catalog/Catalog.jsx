5980b018f3cb95a38fe5c0c353c4574b6c2a7c55
Roman <starsetphenomenon@gmail.com>
2022-08-20 12:10:50 +0300

    adaptive fixes

--- src/Screens/Catalog/Catalog.jsx
+++ src/Screens/Catalog/Catalog.jsx
@@ -1,12 +1,9 @@
+import FilterSelect from '../../Components/FilterSelect/FilterSelect';
 import ItemCard from '../../Components/ItemCard/ItemCard';
 import "./Catalog.scss";
 
 
 function Catalog() {
-
-    const handleVisibility = event => {
-        event.currentTarget.nextElementSibling.classList.toggle('hidden'); // toggle visibility on click
-    };
 
     return (
         <div className='catalog'>
@@ -23,44 +20,8 @@ function Catalog() {
             <h2 className='catalog_heading'>Ножи</h2>
             <div className="content">
                 <div className="filters">
-                    <div className="filter">
-                        <div className="nav" onClick={handleVisibility}>
-                            <h3>Новинки</h3>
-                            <div className="arrow">
-                                <svg className="arrow-down" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M16.7072 7.70725L10.7073 13.7073C10.3163 14.0983 9.68425 14.0983 
-                                        9.29325 13.7073L3.29325 7.70725C2.90225
-                                         7.31625 2.90225 6.68425 3.29325 6.29325C3.68425 5.90225 4.31625 5.90225 4.70725 6.29325L10.0002 11.5862L15.2933
-                                          6.29325C15.6842 5.90225 16.3162 5.90225 16.7072 6.29325C16.9022 6.48825 17.0002 6.74425 17.0002 7.00025C17.0002 
-                                          7.25625 16.9022 7.51225 16.7072 7.70725Z" fill="#828282" />
-                                </svg>
-                            </div>
-                        </div>
-                        <ul className="links hidden">
-                            <li><a href=".">Складной нож</a></li>
-                            <li><a href=".">Складной нож</a></li>
-                            <li><a href=".">Складной нож</a></li>
-                        </ul>
-                    </div>
-                    <div className="filter">
-                        <div className="nav" onClick={handleVisibility}>
-                            <h3>Новинки</h3>
-                            <div className="arrow">
-                                <svg className="arrow-down" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M16.7072 7.70725L10.7073 13.7073C10.3163 14.0983 9.68425 14.0983 
-                                        9.29325 13.7073L3.29325 7.70725C2.90225
-                                         7.31625 2.90225 6.68425 3.29325 6.29325C3.68425 5.90225 4.31625 5.90225 4.70725 6.29325L10.0002 11.5862L15.2933
-                                          6.29325C15.6842 5.90225 16.3162 5.90225 16.7072 6.29325C16.9022 6.48825 17.0002 6.74425 17.0002 7.00025C17.0002 
-                                          7.25625 16.9022 7.51225 16.7072 7.70725Z" fill="#828282" />
-                                </svg>
-                            </div>
-                        </div>
-                        <ul className="links hidden">
-                            <li><a href=".">Складной нож</a></li>
-                            <li><a href=".">Складной нож</a></li>
-                            <li><a href=".">Складной нож</a></li>
-                        </ul>
-                    </div>
+                    <FilterSelect />
+                    <FilterSelect />
                 </div>
                 <div className="items">
                     <div className="items__wrapper">
