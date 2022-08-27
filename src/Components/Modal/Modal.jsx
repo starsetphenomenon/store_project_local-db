93bb4b352671781fb448cfa689eb2a90ec748ed0
Roman <starsetphenomenon@gmail.com>
2022-08-27 17:48:16 +0300

    Modal animation fixed + regEx input patterns

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,17 +1,16 @@
 import './Modal.scss';
-
 import React from 'react'
 
 export default function Modal({ children, modal, setModal }) {
 
     const handleModal = () => {
-        setModal(prev => !prev);
+        setModal(prev => !prev)
     }
 
     return (
         <div className={modal ? 'Modal active' : 'Modal'}>
             <div className={modal ? 'Modal__wrapper active' : 'Modal__wrapper'}></div>
-            <div className={modal ? 'Modal__content active' : 'Modal__content'}>
+            <div className={modal ? 'Modal__content active' : 'Modal__content inActive'}>
                 <div onClick={handleModal} className="close">
                     <span></span>
                     <span></span>
