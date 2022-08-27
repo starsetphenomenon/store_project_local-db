e0a98a1a6a7a146c489457d283e7caeb59b1cbbd
Roman <starsetphenomenon@gmail.com>
2022-08-27 11:54:56 +0300

    routing added + data context

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,45 @@ import Footer from "./Components/Footer/Footer";
 import Catalog from "./Screens/Catalog/Catalog";
 import Menu from './Components/Menu/Menu';
 import Cart from './Screens/Cart/Cart';
-import { useState } from 'react';
+import { useState, useEffect, createContext } from 'react';
+import { Routes, Route, } from "react-router-dom";
+
+export const DataContext = createContext();
 
 function App() {
 
+  const [data, setData] = useState([]);
+  const [menuLinks, setMenuLinks] = useState([]);
 
-  const cartItems = [{
-    id: 0,
-    imgSrc: './assets/img/knifeCatalog.png',
-    imgAlt: '.',
-    name: 'Нож классного качества Knight light C653',
-    amount: 3,
-    price: 750,
-  },
-  {
-    id: 1,
-    imgSrc: './assets/img/knifeCatalog.png',
-    imgAlt: '.',
-    name: 'Нож классного качества Knight light C653',
-    amount: 5,
-    price: 1250,
-  }]
+  useEffect(() => {
+    fetch("data/data.json")
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result);
+      })
+  }, []);
+
+  useEffect(() => {
+    let result = [];
+    let tempCheck = false;
+    data.map(el => {
+      result.forEach(elem => {
+        if (elem.topic === el.topic) {
+          tempCheck = true;
+          return tempCheck
+        }
+      })
+      if (tempCheck) {
+        return
+      }
+      tempCheck = false;
+      result.push({
+        topic: el.topic,
+        id: el.id,
+      })
+    })
+    setMenuLinks(result);
+  }, [data]);
 
   const [menu, setMenu] = useState(false);
 
@@ -34,14 +52,21 @@ function App() {
   }
 
   return (
+
     <div className="App">
-      <Menu menuStatus={menu} menuVisibility={handleMenuVisibility} />
-      <Header menuVisibility={handleMenuVisibility} />
-      <Cart data={cartItems} />
-      <Main />
-      <Catalog />
-      <Footer />
-    </div>
+      <DataContext.Provider value={data}>
+        <Header menuVisibility={handleMenuVisibility} />
+        <Menu menuLinks={menuLinks} menuStatus={menu} menuVisibility={handleMenuVisibility} />
+        <Routes>
+          <Route path="/" index element={<Main />} />
+          <Route path="catalog" element={<Catalog />}>
+            <Route path=":itemID" element={<Catalog />} />
+          </Route>
+          <Route path="cart" element={<Cart />} />
+        </Routes>
+        <Footer />
+      </DataContext.Provider>
+    </div >
   );
 }
 
